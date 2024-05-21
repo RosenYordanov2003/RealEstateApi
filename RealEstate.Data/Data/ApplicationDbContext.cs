fac744c5856f4673b166fd933632f644d39c483c
Rosen Yordanov <roskata182003@gmail.com>
2024-05-21 18:58:21 +0300

    Add ApplicationDbContext

--- RealEstate.Data/Data/ApplicationDbContext.cs
+++ RealEstate.Data/Data/ApplicationDbContext.cs
@@ -3,10 +3,10 @@
     using Microsoft.AspNetCore.Identity;
     using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
     using Microsoft.EntityFrameworkCore;
+    using RealEstate.Data.Data.Models;
 
-    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    public class ApplicationDbContext : IdentityDbContext<User, IdentityRole<Guid>, Guid>
     {
         public ApplicationDbContext(DbContextOptions options) : base(options) { }
-
     }
 }
