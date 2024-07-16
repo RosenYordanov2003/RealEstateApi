cc49443e20255b4b5cc665d1490f4b696500b50c
RosenYordanov2003 <roskata182003@gmail.com>
2024-07-16 17:06:30 +0300

    Add School files with their name and location

--- RealEstateApi/Controllers/HomeController.cs
+++ RealEstateApi/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿namespace RealEstate.Controllers
 {
+    using System.IO;
     using MediatR;
     using Microsoft.AspNetCore.Mvc;
     using Core.Queries.Cities;
@@ -7,6 +8,7 @@
     using Core.Queries.PropertyCategories;
     using Core.Queries.SaleCategories;
     using RealEstate.Core.Models.Property;
+    using Newtonsoft.Json.Linq;
 
     [Route("api/home")]
     [ApiController]
@@ -58,6 +60,40 @@
             var properties = await _mediator.Send(new GetFilteredPropertiesQuery(filterModel));
 
             return Ok(properties);
+        }
+
+        [HttpGet]
+        [Route("GetJson")]
+        public async Task<IActionResult> GetJson()
+        {
+            string csvOutputFilePath = "C:\\Users\\Home\\source\\repos\\RealEstateApi\\RealEstateApi\\schools.csv"; // Заменете с пътя към вашия CSV изходен файл
+
+
+            string jsonData = await System.IO.File.ReadAllTextAsync("C:\\Users\\Home\\source\\repos\\RealEstateApi\\RealEstateApi\\schools.json");
+            var jsonObject = JObject.Parse(jsonData);
+            var elements = jsonObject["elements"];
+
+            using (var writer = new StreamWriter(csvOutputFilePath))
+            {
+                // Записване на заглавката в CSV файла
+                writer.WriteLine("name,latitude,longitude");
+
+                foreach (var element in elements)
+                {
+                    var tags = element["tags"];
+                    if (tags != null && tags["name"] != null)
+                    {
+                        string name = tags["name"].ToString();
+                        string lat = element["lat"]?.ToString() ?? "";
+                        string lon = element["lon"]?.ToString() ?? "";
+
+                        writer.WriteLine($"{name},{lat},{lon}");
+                    }
+                }
+            }
+
+            Console.WriteLine("Конвертирането завърши успешно.");
+            return Ok();
         }
     }
 }
