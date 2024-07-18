ddbd6b45fa4fcc3e61c85297192fc34193ae7198
RosenYordanov2003 <roskata182003@gmail.com>
2024-07-18 18:16:38 +0300

    Add Subscription db model

---
+++ RealEstate.Data/Data/Models/Subscription.cs
@@ -0,0 +1,11 @@
+﻿namespace RealEstate.Data.Data.Models
+{
+    using System.ComponentModel.DataAnnotations.Schema;
+    public class Subscription
+    {
+        public Guid Id { get; set; }
+        [ForeignKey(nameof(User))]
+        public Guid UserId { get; set; }
+        public User User { get; set; } = null!;
+    }
+}
