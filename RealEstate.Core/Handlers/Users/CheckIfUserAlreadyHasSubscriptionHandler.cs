2ff2934347256671b3e7e9b59450444784f154b7
RosenYordanov2003 <roskata182003@gmail.com>
2024-07-19 13:54:51 +0300

    Add CheckIfUserAlreadyHasSubscriptionHandler to check if user is already subscribed for given property category

---
+++ RealEstate.Core/Handlers/Users/CheckIfUserAlreadyHasSubscriptionHandler.cs
@@ -0,0 +1,22 @@
+﻿namespace RealEstate.Core.Handlers.Users
+{
+    using MediatR;
+    using Queries.Users;
+    using Data.Repositories.Contracts;
+    using Data.Data.Models;
+
+    public class CheckIfUserAlreadyHasSubscriptionHandler : IRequestHandler<CheckIfUserAlreadyHasSubscriptionQuery, bool>
+    {
+        private IUnitOfWork _unitOfWork;
+        public CheckIfUserAlreadyHasSubscriptionHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<bool> Handle(CheckIfUserAlreadyHasSubscriptionQuery request, CancellationToken cancellationToken)
+        {
+            return await _unitOfWork.Repository<Subscription>()
+               .CheckIfExistsByIdAsync(s => s.UserId == request.userId && s.SubscriptionCategoryId == request.propertyCategoryId);
+                
+        }
+    }
+}
