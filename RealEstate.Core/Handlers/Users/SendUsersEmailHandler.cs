a8cb5f3c24c31e2def17073e55fa83611a8993a6
RosenYordanov2003 <roskata182003@gmail.com>
2024-07-19 13:36:51 +0300

    Add SendUsersEmailHandler which sends emails to all subscribed users

---
+++ RealEstate.Core/Handlers/Users/SendUsersEmailHandler.cs
@@ -0,0 +1,22 @@
+﻿namespace RealEstate.Core.Handlers.Users
+{
+    using MediatR;
+    using Queries.Users;
+    using Core.Contracts.Email;
+
+    public class SendUsersEmailHandler : IRequestHandler<SendUsersEmailQuery>
+    {
+        private readonly IEmailSender _emailSender;
+        public SendUsersEmailHandler(IEmailSender emailSender)
+        {
+           _emailSender = emailSender;
+        }
+        public async Task Handle(SendUsersEmailQuery request, CancellationToken cancellationToken)
+        {
+            foreach (string email in request.emails)
+            {
+               await _emailSender.SendEmailAsync(email, "Subscription", "<h1>You have recieved Nottification </h1>");
+            }
+        }
+    }
+}
