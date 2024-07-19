c7135a39c0b109854ca147104612465fbe66240a
RosenYordanov2003 <roskata182003@gmail.com>
2024-07-19 13:36:12 +0300

    Add GetUserEmailsHandler which gets user emails for given subscription category

---
+++ RealEstate.Core/Handlers/Users/GetUserEmailsHandler.cs
@@ -0,0 +1,26 @@
+﻿namespace RealEstate.Core.Handlers.Users
+{
+    using Microsoft.EntityFrameworkCore;
+    using MediatR;
+    using Queries.Users;
+    using Data.Repositories.Contracts;
+    using Data.Data.Models;
+
+    public class GetUserEmailsHandler : IRequestHandler<GetUserEmailsQuery, IEnumerable<string>>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public GetUserEmailsHandler(IUnitOfWork unitOfWork)
+        {
+                _unitOfWork = unitOfWork;
+        }
+        public async Task<IEnumerable<string>> Handle(GetUserEmailsQuery request, CancellationToken cancellationToken)
+        {
+           return await _unitOfWork.Repository<User>()
+                .GetAll(false, u => u.Subscriptions
+                .Any(s => s.SubscriptionCategoryId == request.categoryId))
+                .Select(u => u.Email)
+                .ToArrayAsync();
+        }
+    }
+}
