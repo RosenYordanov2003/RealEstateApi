ab4b1d65014e8721db423e7385527d0864382b3c
RosenYordanov2003 <roskata182003@gmail.com>
2024-07-19 14:17:13 +0300

    Add RemoveSubscriptionHandler which deletes given subscription by userId and subscription category from the database

---
+++ RealEstate.Core/Handlers/Subscription/RemoveSubscriptionHandler.cs
@@ -0,0 +1,26 @@
+﻿namespace RealEstate.Core.Handlers.Subscription
+{
+    using MediatR;
+    using Core.Commands.Subscription;
+    using Data.Repositories.Contracts;
+    using Data.Data.Models;
+    using Microsoft.EntityFrameworkCore;
+
+    public class RemoveSubscriptionHandler : IRequestHandler<RemoveSubscriptionCommand>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public RemoveSubscriptionHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task Handle(RemoveSubscriptionCommand request, CancellationToken cancellationToken)
+        {
+            Subscription subscription  = await _unitOfWork.Repository<Subscription>()
+                .GetAll(false, s => s.SubscriptionCategoryId == request.categoryId && s.UserId == request.userId)
+                .FirstAsync();
+
+            await _unitOfWork.Repository<Subscription>().DeleteAsync(subscription);
+            await _unitOfWork.SaveChangesAsync();
+        }
+    }
+}
