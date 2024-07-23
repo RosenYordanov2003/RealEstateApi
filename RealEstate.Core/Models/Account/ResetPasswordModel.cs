bf550892cc27fa605f1bb0f04a54dd758d637bd1
RosenYordanov2003 <roskata182003@gmail.com>
2024-07-23 15:52:18 +0300

    Add ResetPasswordModel.cs file

---
+++ RealEstate.Core/Models/Account/ResetPasswordModel.cs
@@ -0,0 +1,20 @@
+﻿namespace RealEstate.Core.Models.Account
+{
+    using System.ComponentModel.DataAnnotations;
+    public class ResetPasswordModel
+    {
+       
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        public string Password { get; set; } = null!;
+
+        [Required]
+        [Compare(nameof(Password), ErrorMessage = "Confirm passwrod does not match")]
+        public string ConfirmPassword { get; set; } = null!;
+
+        public string Token { get; set; } = null!;
+    }
+}
