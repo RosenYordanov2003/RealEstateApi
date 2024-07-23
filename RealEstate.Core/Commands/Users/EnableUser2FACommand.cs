377ab2bb2bd70fc371d8cf1bbdf34e84611255ce
RosenYordanov2003 <roskata182003@gmail.com>
2024-07-23 12:34:25 +0300

    Add logic to enable user 2FA auth usign EnableUser2FACommand and EnableUser2FAHandler classes

---
+++ RealEstate.Core/Commands/Users/EnableUser2FACommand.cs
@@ -0,0 +1,7 @@
+﻿
+namespace RealEstate.Core.Commands.Users
+{
+    using MediatR;
+    public record EnableUser2FACommand(string userName) : IRequest;
+
+}
---
+++ RealEstate.Core/Handlers/Users/EnableUser2FAHandler.cs
@@ -0,0 +1,25 @@
+﻿namespace RealEstate.Core.Handlers.Users
+{
+    using Microsoft.EntityFrameworkCore;
+    using MediatR;
+    using Core.Commands.Users;
+    using Data.Repositories.Contracts;
+    using Data.Data.Models;
+
+    public class EnableUser2FAHandler : IRequestHandler<EnableUser2FACommand>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public EnableUser2FAHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task Handle(EnableUser2FACommand request, CancellationToken cancellationToken)
+        {
+            User user = await _unitOfWork.Repository<User>().GetByAsync(u => u.UserName == request.userName).FirstAsync();
+
+            user.TwoFactorEnabled = true;
+
+            await _unitOfWork.SaveChangesAsync();
+        }
+    }
+}
