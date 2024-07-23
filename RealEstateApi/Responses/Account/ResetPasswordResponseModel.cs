0838fcc54ffacee890836794a589cf9c01cb023d
RosenYordanov2003 <roskata182003@gmail.com>
2024-07-23 15:56:52 +0300

    Add ResetPasswordResponseModel.cs file

---
+++ RealEstateApi/Responses/Account/ResetPasswordResponseModel.cs
@@ -0,0 +1,13 @@
+﻿namespace RealEstate.Responses.Account
+{
+    using Core.Models.Account;
+    public class ResetPasswordResponseModel : BaseResponseModel
+    {
+        public ResetPasswordResponseModel(ResetPasswordModel model, string message, bool success) : base(message, success)
+        {
+            Model = model;
+        }
+
+        public ResetPasswordModel Model { get; private set; }
+    }
+}
