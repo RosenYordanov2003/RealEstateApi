27d482365ed72988aa8e37ff68de52eac723ae97
RosenYordanov2003 <roskata182003@gmail.com>
2024-07-24 12:40:26 +0300

    Add logic to check if property is already owned by user

---
+++ RealEstate.Core/Handlers/Properties/CheckIfPropertyIsAlreadyOwnedByUserHandler.cs
@@ -0,0 +1,20 @@
+﻿namespace RealEstate.Core.Handlers.Properties
+{
+    using MediatR;
+    using Core.Queries.Properties;
+    using Data.Data.Models;
+    using Data.Repositories.Contracts;
+    public class CheckIfPropertyIsAlreadyOwnedByUserHandler : IRequestHandler<CheckIfPropertyIsAlreadyOwnedByUserQuery, bool>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public CheckIfPropertyIsAlreadyOwnedByUserHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<bool> Handle(CheckIfPropertyIsAlreadyOwnedByUserQuery request, CancellationToken cancellationToken)
+        {
+            return await _unitOfWork.Repository<Property>().
+                CheckIfExistsByIdAsync(p => p.Id == request.propertyId && p.OwnerId == request.userId);
+        }
+    }
+}
---
+++ RealEstate.Core/Queries/Properties/CheckIfPropertyIsAlreadyOwnedByUserQuery.cs
@@ -0,0 +1,6 @@
+﻿namespace RealEstate.Core.Queries.Properties
+{
+    using MediatR;
+    public record CheckIfPropertyIsAlreadyOwnedByUserQuery(Guid propertyId, Guid userId) : IRequest<bool>;
+
+}
