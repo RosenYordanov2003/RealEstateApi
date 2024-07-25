b8699d6db398649a9ef2d04d0a97725d56366648
RosenYordanov2003 <roskata182003@gmail.com>
2024-07-25 15:23:15 +0300

    Add GetById action in PictureController

--- RealEstateApi/Controllers/PictureController.cs
+++ RealEstateApi/Controllers/PictureController.cs
@@ -8,8 +8,13 @@
     using Core.Models.Pictures;
     using Core.Queries.Properties;
     using Responses;
+    using Core.Queries.Pictures;
+    using Core.Queries.Users;
+    using Extensions;
+    using Responses.Pictures;
 
     [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     [Route("api/pictures")]
     [ApiController]
@@ -35,13 +40,32 @@
             {
                 return NotFound(new BaseResponseModel("property does not exist", false));
             }
-            if(!await _mediator.Send(new CheckIfPropertyIsAlreadyOwnedByUserQuery(model.PropertyId, model.UserId)))
+
+            Guid userId = await _mediator.Send(new GetUserIdQuery(User.GetUserName()));
+
+            if(!await _mediator.Send(new CheckIfPropertyIsAlreadyOwnedByUserQuery(model.PropertyId, userId)))
             {
                 return BadRequest(new BaseResponseModel("User isn't owning that property", false));
             }
             await _mediator.Send(new CreatePictureCommand(_webHostEnvironment.WebRootPath, model.File, model.PropertyId));
 
             return Ok();
+        }
+
+        [HttpGet("{id}")]
+        [AllowAnonymous]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetById([FromRoute] int id)
+        {
+            if(!await _mediator.Send(new CheckIfPictureExistsByIdQuery(id)))
+            {
+                return NotFound(new BaseResponseModel("Picture does not exist", false));
+            }
+
+            var model = await _mediator.Send(new GetPictureByIdQuery(id));
+
+            return Ok(new PictureResponseModel("", true, model));
         }
     }
 }
